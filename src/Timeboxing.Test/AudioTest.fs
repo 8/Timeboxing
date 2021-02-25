29e8258548d10cd1e680b56907c814bbf5627fb9
mdk <mdk@lostindetails.com>
2021-02-25 09:34:16 +0100

    Removes libVLCSharp

--- src/Timeboxing.Test/AudioTest.fs
+++ src/Timeboxing.Test/AudioTest.fs
@@ -1,7 +1,7 @@
 ﻿module Timeboxing.Test.AudioTest
 
 open Xunit
-open LibVLCSharp.Shared
+//open LibVLCSharp.Shared
 
-[<Fact>]
-let ``initialize native libraries`` () = Core.Initialize ()
+//[<Fact>]
+//let ``initialize native libraries`` () = Core.Initialize ()
--- src/Timeboxing/Audio.fs
+++ src/Timeboxing/Audio.fs
@@ -3,61 +3,61 @@
 open System
 open System.IO
 open FSharp.Control.Reactive
-open LibVLCSharp.Shared
-
-module Internal =
-  let init () =
-    try
-      Core.Initialize()
-      new LibVLC() |> Some
-    with _ -> None
-
-  let defaultPath () =
-    Path.Combine (Environment.GetFolderPath(Environment.SpecialFolder.Windows), "Media", "Windows Notify.wav")
-
-  let mediaFromPath (path : string) (vlc : LibVLC) =
-    new Media(vlc, path, FromType.FromPath)
-    
-  let mediaFromStream vlc stream =
-    new Media(vlc, new StreamMediaInput(stream))
-    
-  let mediaFromAsset vlc asset =
-    mediaFromStream vlc (Assets.streamFrom asset)
-    
-  let mediaNotification vlc =
-    lazy mediaFromAsset vlc Assets.notification
-    
-  let playerFrom (media : Media) =
-    new MediaPlayer (media)
-
-  let play (player : MediaPlayer) =
-    player.Stop()
-    player.Play()
-
-open Internal
+//open LibVLCSharp.Shared
+//
+//module Internal =
+//  let init () =
+//    try
+//      Core.Initialize()
+//      new LibVLC() |> Some
+//    with _ -> None
+//
+//  let defaultPath () =
+//    Path.Combine (Environment.GetFolderPath(Environment.SpecialFolder.Windows), "Media", "Windows Notify.wav")
+//
+//  let mediaFromPath (path : string) (vlc : LibVLC) =
+//    new Media(vlc, path, FromType.FromPath)
+//    
+//  let mediaFromStream vlc stream =
+//    new Media(vlc, new StreamMediaInput(stream))
+//    
+//  let mediaFromAsset vlc asset =
+//    mediaFromStream vlc (Assets.streamFrom asset)
+//    
+//  let mediaNotification vlc =
+//    lazy mediaFromAsset vlc Assets.notification
+//    
+//  let playerFrom (media : Media) =
+//    new MediaPlayer (media)
+//
+//  let play (player : MediaPlayer) =
+//    player.Stop()
+//    player.Play()
+//
+//open Internal
+//
+//let setup (state : State) =
+//
+//  let vlc = init ()
+//  let media = vlc |> Option.map mediaNotification
+//  let player = media |> Option.map (fun media -> lazy (media.Value |> playerFrom))
+//  
+//  let subscribe (player : Lazy<MediaPlayer>) =
+//    state.CompletedTimeboxes
+//    |> Observable.pairwise
+//    |> Observable.filter (fun (t1, t2) -> t2 - t1 = 1)
+//    |> Observable.subscribe (fun _ -> play player.Value |> ignore)
+//    
+//  let subscription = player |> Option.map subscribe
+//    
+//  let dispose (disposeable : IDisposable) = disposeable.Dispose()
+//  let disposeAll () =
+//    subscription |> Option.iter dispose
+//    player |> Option.iter (fun l -> dispose l.Value)
+//    media |> Option.iter (fun l -> dispose l.Value)
+//    vlc |> Option.iter dispose
+//
+//  { new IDisposable with member this.Dispose() = disposeAll() }  
 
 let setup (state : State) =
-
-  let vlc = init ()
-  let media = vlc |> Option.map mediaNotification
-  let player = media |> Option.map (fun media -> lazy (media.Value |> playerFrom))
-  
-  let subscribe (player : Lazy<MediaPlayer>) =
-    state.CompletedTimeboxes
-    |> Observable.pairwise
-    |> Observable.filter (fun (t1, t2) -> t2 - t1 = 1)
-    |> Observable.subscribe (fun _ -> play player.Value |> ignore)
-    
-  let subscription = player |> Option.map subscribe
-    
-  let dispose (disposeable : IDisposable) = disposeable.Dispose()
-  let disposeAll () =
-    subscription |> Option.iter dispose
-    player |> Option.iter (fun l -> dispose l.Value)
-    media |> Option.iter (fun l -> dispose l.Value)
-    vlc |> Option.iter dispose
-
-  { new IDisposable with member this.Dispose() = disposeAll() }  
-
-
-  
+  { new IDisposable with member this.Dispose() = () }
