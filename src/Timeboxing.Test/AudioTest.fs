c9a083c58ed0f873d92e82c2e2998363f87547a6
mdk <mdk@lostindetails.com>
2021-02-25 10:28:52 +0100

    Added basic playback tests

--- src/Timeboxing.Test/AudioTest.fs
+++ src/Timeboxing.Test/AudioTest.fs
@@ -1,7 +1,42 @@
 ﻿module Timeboxing.Test.AudioTest
 
+open System
+open System.IO
+open System.Threading
+open NLayer.NAudioSupport
 open Xunit
-//open LibVLCSharp.Shared
+open FsUnit.Xunit
+open NAudio.Wave
 
-//[<Fact>]
-//let ``initialize native libraries`` () = Core.Initialize ()
+let mp3 = "notification.mp3"
+
+[<Fact>]
+let ``mp3 test file is available`` () =
+  File.Exists(mp3) |> should be True
+
+[<Fact>]
+let ``test default audio output`` () =
+  use out = new WasapiOut()
+  out |> should not' (be Null)
+  ()
+
+[<Fact>]
+let ``load mp3 audio file using NLayer`` () =
+  let builder = Mp3FileReaderBase.FrameDecompressorBuilder(fun wf -> upcast new Mp3FrameDecompressor(wf))
+  use reader = new Mp3FileReaderBase(mp3, builder)
+  ()
+
+[<Fact>]
+let ``play mp3 audio file`` () =
+  use out = new WasapiOut()
+  
+  let builder = Mp3FileReaderBase.FrameDecompressorBuilder(fun wf -> upcast new Mp3FrameDecompressor(wf))
+  use reader = new Mp3FileReaderBase(mp3, builder)
+
+  out.Init(reader)
+  out.Play()
+  
+  while out.PlaybackState = PlaybackState.Playing do
+    Thread.Sleep(TimeSpan.FromMilliseconds(100.))
+  ()
+  
